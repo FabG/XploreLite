9f85c2ae5cb00603e110f96b20ffb001eebfc266
Fabrice Guillaume <fabrice.guillaume@gmail.com>
2012-12-10 22:23:54 -0500

    Initial check-in of module Xplore_Lite

---
+++ FlipsideViewController.cs
@@ -0,0 +1,76 @@
+using MonoTouch.UIKit;
+using System.Drawing;
+using System;
+using MonoTouch.Foundation;
+
+namespace Xplore_Lite
+{
+	public partial class FlipsideViewController : UIViewController
+	{
+		public FlipsideViewController (IntPtr handle) : base (handle)
+		{
+		}
+		
+		public override bool ShouldAutorotateToInterfaceOrientation (UIInterfaceOrientation toInterfaceOrientation)
+		{
+			// Return true for supported orientations
+			return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
+		}
+		
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+		
+		#region View lifecycle
+		
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Any additional setup after loading the view, typically from a nib.
+		}
+		
+		public override void ViewDidUnload ()
+		{
+			base.ViewDidUnload ();
+			
+			// Release any retained subviews of the main view.
+		}
+		
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+		}
+		
+		public override void ViewDidAppear (bool animated)
+		{
+			base.ViewDidAppear (animated);
+		}
+		
+		public override void ViewWillDisappear (bool animated)
+		{
+			base.ViewWillDisappear (animated);
+		}
+		
+		public override void ViewDidDisappear (bool animated)
+		{
+			base.ViewDidDisappear (animated);
+		}
+		
+		#endregion
+		
+		partial void done (UIBarButtonItem sender)
+		{
+			this.DismissModalViewControllerAnimated (true);
+			
+			if (Done != null)
+				Done (this, EventArgs.Empty);
+		}
+		
+		public event EventHandler Done;
+	}
+}
---
+++ MonkeyController.designer.cs
@@ -0,0 +1,17 @@
+// WARNING
+//
+// This file has been generated automatically by MonoDevelop to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+
+namespace Xplore_Lite
+{
+	[Register ("MonkeyController")]
+	partial class MonkeyController
+	{
+		[Action ("clicked:")]
+		partial void clicked (MonoTouch.Foundation.NSObject sender);
+	}
+}
