9f55ede552aeed6cf92ac6052a61e8b83fad4131
Fabrice Guillaume <fabrice.guillaume@gmail.com>
2013-01-03 10:58:28 -0500

    commit with random data and Line chart

---
+++ obj/Xcode/78/VolDistrController.m
@@ -0,0 +1,21 @@
+// WARNING
+// This file has been generated automatically by MonoDevelop to
+// mirror C# types. Changes in this file made by drag-connecting
+// from the UI designer will be synchronized back to C#, but
+// more complex manual changes may not transfer correctly.
+
+
+#import "VolDistrController.h"
+
+@implementation VolDistrController
+@synthesize VDTUIView;
+
+- (void)dealloc {
+    [VDTUIView release];
+    [super dealloc];
+}
+- (void)viewDidUnload {
+    [self setVDTUIView:nil];
+    [super viewDidUnload];
+}
+@end
